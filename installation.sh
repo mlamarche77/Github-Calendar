a7bbe49390b61d55d107c869166160012bae8c33
mjlomeli <mjlomeli@uci.edu>
2022-04-06 18:42:37 -0700

    major upgrades

--- installation.sh
+++ installation.sh
@@ -3,9 +3,13 @@ apt upgrade
 apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
 apt install python3-venv nginx
 apt install git curl build-essential python3-certbot-nginx
+apt autoremove
 pip3 install wheel gunicorn flask matplotlib numpy requests pandas
 pip install wheel gunicorn flask matplotlib numpy requests pandas
-apt autoremove
+pip3 install wheel gunicorn flask matplotlib numpy requests pandas --user
+pip install wheel gunicorn flask matplotlib numpy requests pandas --user
+sudo -H pip3 install wheel gunicorn flask matplotlib numpy requests pandas
+sudo -H pip install wheel gunicorn flask matplotlib numpy requests pandas
 python3 -m venv /home/Github-Calendar/env
 source /home/Github-Calendar/env/bin/activate
 pip3 install wheel gunicorn flask matplotlib numpy requests pandas
--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+chmod 0755 Github-Calendar
 rm /etc/systemd/system/github_calendar.service
 printf "[Unit]\nDescription=Gunicorn instance to serve Github-Calendar\nAfter=network.target\n\n[Service]\nUser=root\nGroup=www-data\nWorkingDirectory=/home/Github-Calendar\nEnvironment=\"PATH=/home/Github-Calendar/env/bin\"\nExecStart=/home/Github-Calendar/bin/gunicorn --workers 3 --bind unix:github_calendar.sock -m 007 wsgi:app\n\n[Install]\nWantedBy=multi-user.target" >> /etc/systemd/system/github_calendar.service
 rm /etc/systemd/system/multi-user.target.wants/github_calendar.service
