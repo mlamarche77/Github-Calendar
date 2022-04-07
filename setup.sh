edb5dfd6c0e4b2b70f9e809e1ba1d15df61da4dc
mjlomeli <mjlomeli@uci.edu>
2022-04-06 20:48:25 -0700

    major upgrades

--- setup.sh
+++ setup.sh
@@ -12,5 +12,4 @@ systemctl daemon-reload
 rm /etc/nginx/sites-available/github_calendar
 rm /etc/nginx/sites-enabled/github_calendar
 printf "server {\n    listen 80;\n    server_name 137.184.84.145 appacademycoaches.com www.appacademycoaches.com;\n\n    location / {\n        include proxy_params;\n        proxy_pass http://unix:/home/Github-Calendar/github_calendar.sock;\n    }\n}" >> /etc/nginx/sites-available/github_calendar
-systemctl status github_calendar
-reboot
+sudo reboot
