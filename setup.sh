cc068d5df7033e77e13f63ffad60e0f3f58b7ed3
mjlomeli <mjlomeli@uci.edu>
2022-04-06 17:41:02 -0700

    major upgrades

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 rm /etc/systemd/system/github_calendar.service
 rm /etc/systemd/system/multi-user.target.wants/github_calendar.service
-echo -e "[Unit]\nDescription=Gunicorn instance to serve Github-Calendar\nAfter=network.target\n\n[Service]\nUser=root\nGroup=www-data\nWorkingDirectory=/home/Github-Calendar\nEnvironment=\"PATH=/home/Github-Calendar/env/bin\"\nExecStart=/home/Github-Calendar/bin/gunicorn --workers 3 --bind unix:app.sock -m 007 wsgi:app\n\n[Install]\nWantedBy=multi-user.target" >> /etc/systemd/system/github_calendar.service
+echo -e "[Unit]\nDescription=Gunicorn instance to serve Github-Calendar\nAfter=network.target\n\n[Service]\nUser=root\nGroup=www-data\nWorkingDirectory=/home/Github-Calendar\nEnvironment=\"PATH=/home/Github-Calendar/env/bin\"\nExecStart=/home/Github-Calendar/bin/gunicorn --workers 3 --bind unix:github_calendar.sock -m 007 wsgi:app\n\n[Install]\nWantedBy=multi-user.target" >> /etc/systemd/system/github_calendar.service
 systemctl start github_calendar
 systemctl enable github_calendar
 rm /etc/nginx/sites-available/github_calendar
