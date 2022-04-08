bcc9d7650f2f5c282bd03f971bbff7d81803e566
mjlomeli <mjlomeli@uci.edu>
2022-04-08 00:21:04 -0700

    major upgrades

--- setup.sh
+++ setup.sh
@@ -11,5 +11,5 @@ sudo systemctl enable github_calendar
 sudo systemctl daemon-reload
 sudo rm /etc/nginx/sites-available/github_calendar
 sudo rm /etc/nginx/sites-enabled/github_calendar
-sudo bash -c 'echo -e "server {\n    listen 80;\n    server_name 137.184.84.145 appacademycoaches.com www.appacademycoaches.com;\n\n    location / {\n        include proxy_params;\n        proxy_pass http://unix:/home/Github-Calendar/github_calendar.sock;\n    }\n}" > /etc/nginx/sites-available/github_calendar'
+sudo bash -c 'echo -e "server {\n    listen 80;\n    listen [::]:80;\n    server_name 137.184.84.145 appacademycoaches.com www.appacademycoaches.com;\n\n    location / {\n        include proxy_params;\n        proxy_pass http://unix:/home/Github-Calendar/github_calendar.sock;\n    }\n}" > /etc/nginx/sites-available/github_calendar'
 sudo reboot
