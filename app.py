c6f7163ec88c66728abfa2f00bd88e6de000383a
mjlomeli <mjlomeli@uci.edu>
2022-04-22 11:27:31 -0700

    split files into folders

--- app.py
+++ app.py
@@ -3,8 +3,15 @@ from flask import Flask
 app = Flask(__name__)
 app.secret_key = "FkDAg7MUxxAuXe3WYICZwg"
 
-from controllers import *
+
+from controllers import home
+from controllers import contribution
+from controllers import upload
+from controllers import authenticated
+from controllers import updates
+from controllers import create_session
 
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
+
